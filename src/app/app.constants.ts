16ec05a569ec23629222ce93f450192f76cb9cae
jsun <jsun@choice.com.au>
2020-06-11 12:29:29 +1000

    gallery thumbnail

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,3 @@
-const assetsPath = './assets/images/';
 export const skillChartLabels = ['JavaScript/TypeScript', 'HTML', 'Angular', 'React', 'CSS', 'Node.js', '.Net'];
 export const skillsData = [90, 80, 85, 70, 85, 70, 70];
 export const cards = [
